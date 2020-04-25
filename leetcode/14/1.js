0d09436c24ad6546811e8fa242420e3c32b8f9ca
guanliping <2289029828@qq.com>
2020-04-25 11:26:18 +0800

    update 最长公共前缀

--- leetcode/14/1.js
+++ leetcode/14/1.js
@@ -20,26 +20,62 @@
 //     return strs[min]
 // };
 
+// var longestCommonPrefix = function(strs) {
+//     let n = strs.length;
+//     if(n == 0) {//判断是否为空
+//         return "";
+//         }
+//         strs.sort();//排序
+//         let first = strs[0];
+//         let last = strs[n-1];
+//         for(var i = 1; i < strs.length; i++) {
+          
+//             for(var j = 0; j< first.length && last.length; j++){
+//                 if(first.charAt(j) !== last.charAt(j)) break;
+//             }
+//             first = first.substr(0, j);           
+//         }
+//         return first;
+// };
+
+//有错误
+// var longestCommonPrefix = function(strs) {
+//     let n = strs.length;
+//     if(n == 0) {//判断是否为空数组
+//         return "";
+//         }
+//         strs.sort();//给字符串排序
+//         let first = strs[0];
+//         let last = strs[n-1];
+//         let min = Math.min(first.length(), last.length());//返回最小值 作为遍历边界
+//         let myHeaders = new Headers();
+//         for(let i = 0; i < min; i++) {
+//             if(first.charAt(i) == last.charAt(i)){
+//                 myHeaders.append(first.charAt(i));//添加值
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         return myHeaders.tostring()
+// };
+
+
+//最优解
 var longestCommonPrefix = function(strs) {
     let n = strs.length;
-    if(n == 0) {//判断是否为空数组
+    if(n == 0) {//判断是否为空
         return "";
         }
-        strs.sort();//给字符串排序
+        strs.sort();//排序
         let first = strs[0];
         let last = strs[n-1];
-        let min = Math.min(first.length(), last.length());//返回最小值 作为遍历边界
-        let myHeaders = new Headers();
-        for(let i = 0; i < min; i++) {
-            if(first.charAt(i) == last.charAt(i)){
-                myHeaders.append(first.charAt(i));//添加值
+        let exp = new RegExp(`^${first}`)
+        //判断是否存在包含关系
+        if (first === last || last.match(exp)) return first;
+        for(let i=0; i<first.length; i++){
+            if(first[i] !== last[i]) {
+                return first.substring(0,i)//不相等返回相应字符串
             }
-            else{
-                break;
-            }
-
         }
-        return myHeaders.tostring()
-
-
 };
