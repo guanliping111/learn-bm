6752d27651b5abcf81e8faf3e04c41035ecc764c
guanliping <2289029828@qq.com>
2020-05-25 12:40:28 +0800

    update 二分查找插入

--- leetcode/35二分查找-搜索插入位置/插入.js
+++ leetcode/35二分查找-搜索插入位置/插入.js
@@ -4,13 +4,24 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-    if (target > nums[right]) return right + 1;
-    while (left < right) {
-        var index = parseInt((left + right) >>> 1);//取左中位数
-        if (nums[index] < target) left = index + 1; //中位数小于目标值，削去区间左侧
-        else right = index; //中位数大于等于目标值，削去区间右侧
+    let index;
+    let low = 0;
+    let high = nums.length - 1;
+    while(low <=high){
+        let mid = Math.floor((low+high)/2);
+        let guess = nums[mid];
+        if(guess === target){
+            index = mid;
+            return index;
+        }
+        else if(guess > target){
+            high = mid -1;
+            index = mid;
+        }
+        else{
+            low = mid +1;
+            index = low;
+        }
     }
-    return left;
+    return index;
 };
