17bf6d0226598d72d9eedfa2fa55dec9fe097284
guanliping <2289029828@qq.com>
2020-05-31 11:03:48 +0800

    update 解析DOM树

--- node/http-browser-process/parse-dom.js
+++ node/http-browser-process/parse-dom.js
@@ -1,6 +1,8 @@
 let htmlStr = `<html>
   <head></head>
   <body>
+    <img />
+    <span></span>
     <div></div>
   </body>
 </html>`
@@ -9,13 +11,13 @@ let htmlStr = `<html>
 // token: html tag
 
 let currentToken = null;
-//栈
+//声明栈
 let stack = [ {type: 'document', children: []} ]
 parse(htmlStr);
 console.log(JSON.stringify(stack[0], null, 2));
 function emit(token) {
   //1：遇到开始标签入栈，同时把这个标签最作为栈顶元素的字节点
-  //2：遇到结束标签就 出栈
+  //2：遇到结束标签就 出栈   
   let top = stack[stack.length - 1];
   if(token.type === 'startTag') {//开始标签处理配对
     // push  pop 
@@ -25,12 +27,24 @@ function emit(token) {
       attributes: [],
       tagName: token.tagName
     }
-    stack.push(element);
+    stack.push(element); //入栈
     // 作为栈顶的元素子节点，为了生成树
     // if (!top.children) top.children = [];
     top.children.push(element);
   }else if(token.type === 'endTag') {//结束标签
-    stack.pop();
+    if(token.tagName !== top.tagName) { //判断标签是否配对
+      throw new Error('tagName match error')
+    }else{
+      stack.pop(); //配对成功 移除
+    }
+  } else if (token.type === 'selfCloseToken') {
+    let element = {
+      type: 'element',
+      children: [],
+      attributes: [],
+      tagName: token.tagName
+    }
+    top.children.push(element);
   }
   currentToken = null;
 }
@@ -66,12 +80,21 @@ function tagName(c) {
   if (c.match(/[a-zA-Z]/)) {
     currentToken.tagName += c;
     return tagName
+  } else if (c.match(/[\t\n\f ]/)) {
+    return beforeAttaibuteName
   } else if (c === '>') {
     // tag 拼接结束
     emit(currentToken);
     return start
   }
 }
+function beforeAttaibuteName(c) {
+  if (c === '/') {//自闭合标签
+    currentToken.type = 'selfCloseToken';
+    return tagName;
+  }
+}
+
 function endTagOpen(c) {
   if (c.match(/[a-zA-Z]/)) {
     currentToken = {
