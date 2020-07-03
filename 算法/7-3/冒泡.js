7d11bdc00ed1942c734ab4d4d3cfaa1737a7a7cb
guanliping <2289029828@qq.com>
2020-07-03 17:01:28 +0800

    add 算法

---
+++ 算法/7-3/冒泡.js
@@ -0,0 +1,19 @@
+let array = [1,21,5,8,38,18,9];
+
+function bSort(array) {
+    for(let i = array.length - 1; i > 0; i --){
+        for(let j = 0; j < i; j ++) {
+            if(array[j] > array[j + 1]) {
+                swap(array, j, j+1);
+            }
+        }
+    }
+    return array;
+}
+
+function swap(array, left, right) {
+    let rightValue = array[right];
+    array[right] = array[left];
+    array[left] = rightValue;
+}
+console.log(bSort(array));
